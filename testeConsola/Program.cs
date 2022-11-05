74a4b7d43313ecf9552f770aca9247764dde90ef
NunoA21 <nuno21araujo95@gmail.com>
2022-11-05 09:38:59 +0000

    TestConsola Restsharp test

--- testeConsola/Program.cs
+++ testeConsola/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//using Newtonsoft.Json;
+using RestSharp;
+using System.Text.Json;
+using testeConsola;
+
 Console.WriteLine("Hello, World!");
+
+var url = "https://reqres.in/api/users?page=2";
+var client = new RestClient(url);
+var request = new RestRequest(url, Method.Get);
+RestResponse response = await client.ExecuteAsync<Test>(request);
+var restResponse = JsonSerializer.Deserialize<Test>(response?.Content);
+
+Console.WriteLine(response);
+Console.ReadLine();
