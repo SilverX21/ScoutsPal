dfc7891553b7f005240a1d8547b9d654929ba01b
NunoA21 <nuno21araujo95@gmail.com>
2022-11-05 10:26:15 +0000

    Added RestClient

---
+++ testeConsola/RestClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testeConsola
+{
+    public class RestClient
+    {
+        public string? statusCode { get; set; }
+        public string? description { get; set; }
+        public string? requestLocation { get; set; }
+        public Dictionary<string, string>? headers { get; set; }
+    }
+}
