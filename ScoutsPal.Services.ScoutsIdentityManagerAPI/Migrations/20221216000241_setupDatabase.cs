f11409c043934332498049150df24290147f9ef8
NunoA21 <nuno21araujo95@gmail.com>
2022-12-16 00:08:20 +0000

    teste db - portátil

---
+++ ScoutsPal.Services.ScoutsIdentityManagerAPI/Migrations/20221216000241_setupDatabase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScoutsPal.Services.ScoutsIdentityManagerAPI.Migrations
+{
+    public partial class setupDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
